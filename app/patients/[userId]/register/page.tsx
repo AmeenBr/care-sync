ab1b7c73ad2604ff6e15720750a05ac68ffa17ea
AmeenBr <brahamamine@issatso.u-sousse.tn>
2024-08-05 11:06:31 +0100

    fixed some classNames in order to get the proper design of the page I need

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import RegisterForm from '@/components/forms/RegisterForm'
-import { getUser } from '@/lib/actions/patient.actions' 
+import { getUser } from '@/lib/actions/patient.actions'
 
-const Register =async ({params: {userId} }: SearchParamProps) => {
-    
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+
     const user = await getUser(userId);
 
     return (
         <div className="flex h-screen max-h-screen">
-          
 
-            <section className="remove-scrollbar container my-auto">
-                <div className="sub-container max-w-[496px]">
+
+            <section className="remove-scrollbar container ">
+                <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
                     <Image
                         src="/assets/icons/logo-full.svg"
                         height={1000}
@@ -21,17 +21,11 @@ const Register =async ({params: {userId} }: SearchParamProps) => {
                         alt="patient"
                         className="mb-12 h-10 w-fit"
                     />
-                    <RegisterForm user={user}/>
+                    <RegisterForm user={user} />
 
-                    <div className='text-14-regular'>
-                        <p className='justify-items-end text-dark-600 xl: text-left'>
-                            © 2024 CareSync
-                        </p>
-                        <Link href="/?admin=true" className="text-green-500">
-                            Admin
-                        </Link>
-                    </div>
-
+                    <p className='copyright py-12'>
+                        © 2024 CareSync
+                    </p>
                 </div>
 
             </section>
