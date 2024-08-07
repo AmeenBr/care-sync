08bae0829be4b05d5bc405322bbda9c1900e0a07
AmeenBr <brahamamine@issatso.u-sousse.tn>
2024-08-07 13:09:47 +0100

    made a simple change in constants/index.ts in order to match the serverside configs (appwrite)

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,6 +1,18 @@
+"use server";
 import { ID, Query } from "node-appwrite";
-import { users } from "../appwrite.config";
+import { InputFile } from "node-appwrite/file";
+import {
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  DATABASE_ID,
+  BUCKET_ID,
+  PROJECT_ID,
+  storage,
+  users,
+} from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { databases } from "../appwrite.config";
+
 export const createUser = async (user: CreateUserParams) => {
   try {
     console.log("Creating user with params:", user);
@@ -28,4 +40,47 @@ export const getUser = async (userId: string) => {
   } catch (error) {
     console.error(error);
   }
-}
+};
+
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
+  try {
+    let file;
+    if (identificationDocument) {
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument?.get("blobFile") as Blob,
+        identificationDocument?.get("fileName") as string
+      );
+
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+    }
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id || null,
+        identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+        ...patient
+      }
+    );
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [Query.equal("userId", userId)]
+    );
+    return parseStringify(patients.documents[0]);
+  } catch (error) {
+    console.error(error);
+  }
+};
